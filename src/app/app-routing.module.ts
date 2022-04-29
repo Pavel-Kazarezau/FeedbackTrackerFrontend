d3688782351aac32d2496d817ffc4253072a6d90
paka <p.kazarezov@gmail.com>
2022-04-29 23:27:03 +0300

    Add home page route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 const routes: Routes = [
-  {path: '', component: MainLayoutComponent}
+  {path: '', component: MainLayoutComponent, children: [
+    {path: '', component: LoginPageComponent},
+    {path: 'home', component: HomePageComponent}
+  ]}
 ];
 
 @NgModule({
