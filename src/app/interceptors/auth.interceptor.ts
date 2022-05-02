5a82949e527f8c6a46f935893f3b8a560a3bb74a
paka <p.kazarezov@gmail.com>
2022-05-02 23:32:09 +0300

    Add uth interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { AuthenticationService } from "../service/authentication.service";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthenticationService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if(req.url !== '/api/login') {
+            let authReq = req.clone({
+                headers: req.headers.append('Authorization', this.authService.getAuthToken())
+            })
+            return next.handle(authReq)
+        }
+        return next.handle(req)
+    }
+
+}
