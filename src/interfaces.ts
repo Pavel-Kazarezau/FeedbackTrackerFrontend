2d77b394aaf37c1b0ab2008e2f2a24b3208334ef
paka <p.kazarezov@gmail.com>
2022-05-02 13:52:32 +0300

    Add Feedback item model

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,3 +3,21 @@ export interface User {
     password: string,
     _links?: any
 }
+
+export interface FeedbackItem {
+    id: number,
+    text: string,
+    userId: number,
+    responseItem: ResponseItem
+    isCompleted: boolean,
+    _links: {
+        self: {
+            href: string
+        }
+    }
+}
+
+export interface ResponseItem {
+    feedbackItemId: number,
+    response: string
+}
