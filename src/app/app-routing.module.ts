50ab2003972c62e849c25d2911a27561611ec3cb
paka <p.kazarezov@gmail.com>
2022-05-08 16:57:56 +0300

    Add redirect to home page if route is not created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FeedbackItemPageComponent } from './feedback-item-page/feedback-item-page.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FeedbackItemPageComponent} from './feedback-item-page/feedback-item-page.component';
+import {HomePageComponent} from './home-page/home-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
 import {AuthGuard} from "./guard/auth.guard";
 
 const routes: Routes = [
-  {path: '', component: MainLayoutComponent, children: [
-    {path: '', component: LoginPageComponent},
-    {path: 'home', component: HomePageComponent, canActivate: [AuthGuard]},
-    {path: 'feedback/:id', component: FeedbackItemPageComponent, canActivate: [AuthGuard]},
-    {path: 'unresolved', component: HomePageComponent, canActivate: [AuthGuard]}
-  ]}
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', component: LoginPageComponent},
+      {path: 'home', component: HomePageComponent, canActivate: [AuthGuard]},
+      {path: 'feedback/:id', component: FeedbackItemPageComponent, canActivate: [AuthGuard]},
+      {path: 'unresolved', component: HomePageComponent, canActivate: [AuthGuard]},
+      {path: '**', redirectTo: 'home'}
+    ]
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
